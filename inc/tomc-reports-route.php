4bcdc77d6edce96a786157fe4a2100d1b97168e3
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2025-07-16 13:37:33 -0400

    Add files via upload

--- inc/tomc-reports-route.php
+++ inc/tomc-reports-route.php
@@ -60,7 +60,7 @@ function getPayoutRecords($data){
         join %i um on users.id = um.user_id
         and um.meta_key = "_vendor_commission"
         where completed.date_created_gmt >= %s
-        and completed.date_created_gmt <= %s;
+        and completed.date_created_gmt <= %s
         group by users.display_name, CTE.cte_order_id, CTE.cte_order_cost;';
         // $results = $wpdb->get_results($wpdb->prepare($query, $orders_table, $order_items_table, $order_product_lookup_table, $posts_table, $users_table, $item_meta_table, $order_meta_table, $user_meta_table, $startDate, $endDate), ARRAY_A);
         $results = $wpdb->get_results($wpdb->prepare($query, $orders_table, $order_items_table, $order_product_lookup_table, $posts_table, $users_table, $item_meta_table, $order_meta_table, $startDate, $endDate, $orders_table, $order_items_table, $order_product_lookup_table, $posts_table, $users_table, $item_meta_table, $order_meta_table, $user_meta_table, $startDate, $endDate), ARRAY_A);
