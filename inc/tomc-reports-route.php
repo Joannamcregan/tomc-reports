607b93d8fe830df1deb51cd2be4f3e099333d12f
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2025-07-16 21:44:35 -0400

    Add files via upload

--- inc/tomc-reports-route.php
+++ inc/tomc-reports-route.php
@@ -59,7 +59,7 @@ function getPayoutRecords($data){
             where completed.date_created_gmt >= %s
             and completed.date_created_gmt <= %s
         )
-        select users.display_name, sum(line_total.meta_value) as total_revenue, sum(stripe.meta_value * cte1_percent_order_cost) as stripe_fees, ((sum(line_total.meta_value) - sum(stripe.meta_value)) * (um.meta_value / 100)) as commission
+        select users.display_name, sum(line_total.meta_value) as total_revenue, stripe.meta_value * cte1_percent_order_cost as stripe_fees, ((sum(line_total.meta_value) - sum(stripe.meta_value)) * (um.meta_value / 100)) as commission
         from %i completed 
         join %i items on completed.id = items.order_id
         and completed.status = "wc-completed"
