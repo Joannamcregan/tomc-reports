f3feaa9ca90ad36094f713f9bd872ee874047d70
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2025-07-17 07:57:10 -0400

    Add files via upload

--- inc/tomc-reports-route.php
+++ inc/tomc-reports-route.php
@@ -59,7 +59,7 @@ function getPayoutRecords($data){
             where completed.date_created_gmt >= %s
             and completed.date_created_gmt <= %s
         )
-        select users.display_name, sum(line_total.meta_value) as total_revenue, stripe.meta_value * cte1_percent_order_cost as stripe_fees, ((sum(line_total.meta_value) - sum(stripe.meta_value)) * (um.meta_value / 100)) as commission
+        select users.display_name, sum(line_total.meta_value) as total_revenue, sum(stripe.meta_value * cte1_percent_order_cost) as stripe_fees, ((sum(line_total.meta_value) - sum(stripe.meta_value)) * (um.meta_value / 100)) as commission
         from %i completed 
         join %i items on completed.id = items.order_id
         and completed.status = "wc-completed"
@@ -76,7 +76,7 @@ function getPayoutRecords($data){
         and um.meta_key = "_vendor_commission"
         where completed.date_created_gmt >= %s
         and completed.date_created_gmt <= %s
-        group by users.display_name, um.meta_value, cte1_percent_order_cost;';
+        group by users.display_name, um.meta_value, cte1.cte1_percent_order_cost;';
         // $results = $wpdb->get_results($wpdb->prepare($query, $orders_table, $order_items_table, $order_product_lookup_table, $posts_table, $users_table, $item_meta_table, $order_meta_table, $user_meta_table, $startDate, $endDate), ARRAY_A);
         $results = $wpdb->get_results($wpdb->prepare($query, $orders_table, $order_items_table, $order_product_lookup_table, $posts_table, $users_table, $item_meta_table, $order_meta_table, $startDate, $endDate, $orders_table, $order_items_table, $order_product_lookup_table, $posts_table, $users_table, $item_meta_table, $order_meta_table, $startDate, $endDate, $orders_table, $order_items_table, $order_product_lookup_table, $posts_table, $users_table, $item_meta_table, $order_meta_table, $user_meta_table, $startDate, $endDate), ARRAY_A);
         // return $results;
