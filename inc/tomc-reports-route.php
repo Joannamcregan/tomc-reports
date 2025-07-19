94d9e7ee53c93555e725462d95595c80feec9c8f
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2025-07-19 11:27:50 -0400

    Add files via upload

--- inc/tomc-reports-route.php
+++ inc/tomc-reports-route.php
@@ -31,13 +31,8 @@ function getPayoutRecords($data){
             join %i items on completed.id = items.order_id
             and completed.status = "wc-completed"
             and items.order_item_type = "line_item"
-            join %i l on l.order_item_id = items.order_item_id
-            join %i posts on l.product_id = posts.id
-            join %i users on posts.post_author = users.id
             join %i line_total on items.order_item_id = line_total.order_item_id
             and line_total.meta_key = "_line_total"
-            join %i stripe on completed.id = stripe.order_id
-            and stripe.meta_key = "_stripe_fee"
             where completed.date_created_gmt >= %s
             and completed.date_created_gmt <= %s
             group by completed.id
@@ -49,13 +44,8 @@ function getPayoutRecords($data){
             join %i items on completed.id = items.order_id
             and completed.status = "wc-completed"
             and items.order_item_type = "line_item"
-            join %i l on l.order_item_id = items.order_item_id
-            join %i posts on l.product_id = posts.id
-            join %i users on posts.post_author = users.id
             join %i line_total on items.order_item_id = line_total.order_item_id
             and line_total.meta_key = "_line_total"
-            join %i stripe on completed.id = stripe.order_id
-            and stripe.meta_key = "_stripe_fee"
             where completed.date_created_gmt >= %s
             and completed.date_created_gmt <= %s
         ),
@@ -66,15 +56,10 @@ function getPayoutRecords($data){
             and completed.status = "wc-completed"
             and items.order_item_type = "line_item"
             join CTE1 on items.order_item_id = CTE1.cte1_item_id
-            join %i l on l.order_item_id = items.order_item_id
-            join %i posts on l.product_id = posts.id
-            join %i users on posts.post_author = users.id
             join %i line_total on items.order_item_id = line_total.order_item_id
             and line_total.meta_key = "_line_total"
             join %i stripe on completed.id = stripe.order_id
             and stripe.meta_key = "_stripe_fee"
-            join %i um on users.id = um.user_id
-            and um.meta_key = "_vendor_commission"
             where completed.date_created_gmt >= %s
             and completed.date_created_gmt <= %s
         )
@@ -84,22 +69,17 @@ function getPayoutRecords($data){
         and completed.status = "wc-completed"
         and items.order_item_type = "line_item"
         join CTE2 on items.order_item_id = CTE2.cte2_item_id
-        join %i l on l.order_item_id = items.order_item_id
-        join %i posts on l.product_id = posts.id
-        join %i users on posts.post_author = users.id
         join %i line_total on items.order_item_id = line_total.order_item_id
         and line_total.meta_key = "_line_total"
-        join %i stripe on completed.id = stripe.order_id
-        and stripe.meta_key = "_stripe_fee"
-        join %i um on users.id = um.user_id
-        and um.meta_key = "_vendor_commission"
+        join %i item_commission on items.order_item_id = item_commission.order_item_id
+        and item_commission.meta_key = "_vendor_item_commission"
         where completed.date_created_gmt >= %s
         and completed.date_created_gmt <= %s
         group by users.display_name, um.meta_value;';
         // $results = $wpdb->get_results($wpdb->prepare($query, $orders_table, $order_items_table, $order_product_lookup_table, $posts_table, $users_table, $item_meta_table, $order_meta_table, $user_meta_table, $startDate, $endDate), ARRAY_A);
-        $results = $wpdb->get_results($wpdb->prepare($query, $orders_table, $order_items_table, $order_product_lookup_table, $posts_table, $users_table, $item_meta_table, $order_meta_table, $startDate, $endDate, $orders_table, $order_items_table, $order_product_lookup_table, $posts_table, $users_table, $item_meta_table, $order_meta_table, $startDate, $endDate, $orders_table, $order_items_table, $order_product_lookup_table, $posts_table, $users_table, $item_meta_table, $order_meta_table, $user_meta_table, $startDate, $endDate, $orders_table, $order_items_table, $order_product_lookup_table, $posts_table, $users_table, $item_meta_table, $order_meta_table, $user_meta_table, $startDate, $endDate), ARRAY_A);
+        $results = $wpdb->get_results($wpdb->prepare($query, $orders_table, $order_items_table, $item_meta_table, $startDate, $endDate, $orders_table, $order_items_table, $item_meta_table, $startDate, $endDate, $orders_table, $order_items_table, $item_meta_table, $order_meta_table, $startDate, $endDate, $orders_table, $order_items_table, $item_meta_table, $item_meta_table, $startDate, $endDate), ARRAY_A);
         // return $results;
-        return $wpdb->prepare($query, $orders_table, $order_items_table, $order_product_lookup_table, $posts_table, $users_table, $item_meta_table, $order_meta_table, $startDate, $endDate, $orders_table, $order_items_table, $order_product_lookup_table, $posts_table, $users_table, $item_meta_table, $order_meta_table, $startDate, $endDate, $orders_table, $order_items_table, $order_product_lookup_table, $posts_table, $users_table, $item_meta_table, $order_meta_table, $user_meta_table, $startDate, $endDate, $orders_table, $order_items_table, $order_product_lookup_table, $posts_table, $users_table, $item_meta_table, $order_meta_table, $user_meta_table, $startDate, $endDate);
+        return $wpdb->prepare($query, $orders_table, $order_items_table, $item_meta_table, $startDate, $endDate, $orders_table, $order_items_table, $item_meta_table, $startDate, $endDate, $orders_table, $order_items_table, $item_meta_table, $order_meta_table, $startDate, $endDate, $orders_table, $order_items_table, $item_meta_table, $item_meta_table, $startDate, $endDate);
     } else {
         wp_safe_redirect(site_url('/my-account'));
         return 'fail';
