5061505103bf15aec03792bd52ec891166debc38
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2025-07-19 11:52:23 -0400

    Add files via upload

--- inc/tomc-reports-route.php
+++ inc/tomc-reports-route.php
@@ -63,23 +63,26 @@ function getPayoutRecords($data){
             where completed.date_created_gmt >= %s
             and completed.date_created_gmt <= %s
         )
-        select users.display_name, sum(line_total.meta_value) as total_revenue, sum(stripe_fee) as stripe_fees, ((sum(line_total.meta_value) - sum(stripe.meta_value)) * (um.meta_value / 100)) as commission
+        select users.display_name, sum(line_total.meta_value) as total_revenue, sum(stripe_fee) as stripe_fees, sum(item_commission.meta_value) as commission
         from %i completed 
         join %i items on completed.id = items.order_id
         and completed.status = "wc-completed"
         and items.order_item_type = "line_item"
         join CTE2 on items.order_item_id = CTE2.cte2_item_id
+        join %i l on l.order_item_id = items.order_item_id
+        join %i posts on l.product_id = posts.id
+        join %i users on posts.post_author = users.id
         join %i line_total on items.order_item_id = line_total.order_item_id
         and line_total.meta_key = "_line_total"
         join %i item_commission on items.order_item_id = item_commission.order_item_id
         and item_commission.meta_key = "_vendor_item_commission"
         where completed.date_created_gmt >= %s
         and completed.date_created_gmt <= %s
-        group by users.display_name, um.meta_value;';
+        group by users.display_name;';
         // $results = $wpdb->get_results($wpdb->prepare($query, $orders_table, $order_items_table, $order_product_lookup_table, $posts_table, $users_table, $item_meta_table, $order_meta_table, $user_meta_table, $startDate, $endDate), ARRAY_A);
-        $results = $wpdb->get_results($wpdb->prepare($query, $orders_table, $order_items_table, $item_meta_table, $startDate, $endDate, $orders_table, $order_items_table, $item_meta_table, $startDate, $endDate, $orders_table, $order_items_table, $item_meta_table, $order_meta_table, $startDate, $endDate, $orders_table, $order_items_table, $item_meta_table, $item_meta_table, $startDate, $endDate), ARRAY_A);
+        $results = $wpdb->get_results($wpdb->prepare($query, $orders_table, $order_items_table, $item_meta_table, $startDate, $endDate, $orders_table, $order_items_table, $item_meta_table, $startDate, $endDate, $orders_table, $order_items_table, $item_meta_table, $order_meta_table, $startDate, $endDate, $orders_table, $order_items_table, $order_product_lookup_table, $posts_table, $users_table, $item_meta_table, $item_meta_table, $startDate, $endDate), ARRAY_A);
         // return $results;
-        return $wpdb->prepare($query, $orders_table, $order_items_table, $item_meta_table, $startDate, $endDate, $orders_table, $order_items_table, $item_meta_table, $startDate, $endDate, $orders_table, $order_items_table, $item_meta_table, $order_meta_table, $startDate, $endDate, $orders_table, $order_items_table, $item_meta_table, $item_meta_table, $startDate, $endDate);
+        return $wpdb->prepare($query, $orders_table, $order_items_table, $item_meta_table, $startDate, $endDate, $orders_table, $order_items_table, $item_meta_table, $startDate, $endDate, $orders_table, $order_items_table, $item_meta_table, $order_meta_table, $startDate, $endDate, $orders_table, $order_items_table, $order_product_lookup_table, $posts_table, $users_table, $item_meta_table, $item_meta_table, $startDate, $endDate);
     } else {
         wp_safe_redirect(site_url('/my-account'));
         return 'fail';
