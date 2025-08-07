07f375a652e52d5b9894927878ab92788223f65a
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2025-08-07 13:45:10 -0400

    Add files via upload

--- inc/tomc-reports-route.php
+++ inc/tomc-reports-route.php
@@ -85,8 +85,8 @@ function getPayoutRecords($data){
         join %i um on users.id = um.user_id
         and um.meta_key = "_vendor_commission"
         where child_order.date_created_gmt >= %s
-        and child_order.date_created_gmt <= %s';
-        // group by users.display_name;';
+        and child_order.date_created_gmt <= %s
+        group by users.display_name;';
         // $results = $wpdb->get_results($wpdb->prepare($query, $orders_table, $order_items_table, $order_product_lookup_table, $posts_table, $users_table, $item_meta_table, $order_meta_table, $user_meta_table, $startDate, $endDate), ARRAY_A);
         $results = $wpdb->get_results($wpdb->prepare($query, $orders_table, $orders_table, $order_items_table, $item_meta_table, $startDate, $endDate, $orders_table, $orders_table, $order_items_table, $item_meta_table, $startDate, $endDate, $orders_table, $orders_table, $order_items_table, $item_meta_table, $order_meta_table, $startDate, $endDate, $orders_table, $orders_table, $order_items_table, $order_product_lookup_table, $posts_table, $users_table, $item_meta_table, $item_meta_table, $user_meta_table, $startDate, $endDate), ARRAY_A);
         // return $results;
